688333e7b8afb7434eae9ba75cb992480fdf3f9a
danielpgp1012 <daniel.pachecogutierrez@mail.mcgill.ca>
2021-08-21 16:55:44 +0200

    Deploy to azure websites

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,21 @@
 from flask_restplus import Api
+from flask import url_for
 from .formulario_ziu_api import formulario_ns
+import os
 
+if not os.environ.get("ENV"):
+
+    @property
+    def specs_url(self):
+        return url_for(self.endpoint("specs"), _external=True, _scheme="https")
+
+    Api.specs_url = specs_url
 
 api = Api(
     title="ZIU Codigo QR generador API",
     version="1.0",
     description="Esta API va a crear codigos QR para \
-    las personas que asistan al seminario en Palexpo en 2021.",
+    las personas que asistan al seminario de ZIU en Palexpo en 2021.",
 )
 
 api.add_namespace(formulario_ns)
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, url_for
 from api import api
 import os
 
 app = Flask(__name__)
+
 api.init_app(app)
-# app.run(debug=True,host='0.0.0.0')
-app.run(debug=True)
-# if __name__ == '__main__':
+# app.run(debug=True, host="0.0.0.0")
+
+if __name__ == "__main__":
+    app.run(debug=True)
